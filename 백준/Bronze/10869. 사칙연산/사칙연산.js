614dcf04789704bd7e1ea7aa4c099aac5adc33c0
Jootak Kim <83898103+Taak-e@users.noreply.github.com>
2022-07-23 16:57:18 +0900

    [Bronze V] Title: 사칙연산, Time: 132 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ').map(value => +value)
+
+const [a, b] = input
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
